6c5df26b7afc421292845c8b02143ccba0f6a256
The Anger <anger@floatbetween.space>
2025-05-14 13:02:30 +1000

    Update rikki_kuu_mod.gd
    made conformant to script template

--- rikki_kuu_mod.gd
+++ rikki_kuu_mod.gd
@@ -1,7 +1,27 @@
+#@icon
 class_name RikkiKuuMod
 extends RefCounted
 ## Main mod interface for Rikki Kuu
 
+# signals
+# enums
+# constants
+# static variables
+# @export variables
+# remaining regular variables
+# @onready variables
+
+# _static_init()
+# remaining static methods
+# _init()
+# _enter_tree()
+# _ready()
+# _process()
+# _physics_process()
+# remaining virtual methods
+# overridden custom methods
+
+#region remaining methods
 
 ## the internal name of this mod, must match its folder in res://mods/
 func get_shortname() -> String:
@@ -21,3 +41,7 @@ func get_required_mods() -> Array[String]:
 ## mods that if available must be loaded before this mod can load
 func get_optional_mods() -> Array[String]:
 	return []
+
+#endregion
+
+# subclasses
